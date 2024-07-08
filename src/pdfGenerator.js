f486be053219295278acdf5a286801ce492dbc50
Ezequiel De La Rosa <linezekdlr@hotmail.com>
2024-07-08 15:24:01 +0200

    refactor: rename functionality

---
+++ src/pdfGenerator.js
@@ -0,0 +1,27 @@
+import { getDomainName } from "./utils/getDomainName.js";
+
+import { chromium } from "playwright/test";
+
+import { fileURLToPath } from 'url';
+import { dirname, join, normalize } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const pdfsFolders = join(__dirname, '.', 'pdfs');
+
+export default async function generatePdfWithPlaywright({url}) {
+    const browser = await chromium.launch({
+        headless: true,
+    });
+
+    const page = await browser.newPage();
+    await page.goto(url);
+    await page.emulateMedia({ media: 'screen' });
+
+    const pathToSavePdf = normalize(pdfsFolders + '/' + getDomainName(url) + '.pdf');
+    const pdf = await page.pdf({ path: pathToSavePdf, format: 'A4', printBackground: true});
+    
+    await browser.close();
+    return pdf;
+}
+
