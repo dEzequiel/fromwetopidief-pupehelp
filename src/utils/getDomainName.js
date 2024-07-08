ee187805aeef6564579597e05c9f8e512b26401b
Ezequiel De La Rosa <linezekdlr@hotmail.com>
2024-07-08 15:23:50 +0200

    feat: util to get domain name

---
+++ src/utils/getDomainName.js
@@ -0,0 +1,10 @@
+export function getDomainName(url) {
+    const urlO = new URL(url);
+    let domain = urlO.hostname;
+
+    if (domain.startsWith('www.')) {
+        domain = domain.substring(4);
+      }
+
+    return domain;
+}
